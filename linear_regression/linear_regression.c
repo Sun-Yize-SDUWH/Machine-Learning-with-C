0bb065276ca4949a53d9deba6e22a6caad89a4cc
Charlie <986765210@qq.com>
2020-04-20 16:12:27 +0800

    linear_regression

--- linear_regression/linear_regression.c
+++ linear_regression/linear_regression.c
@@ -1,8 +1,12 @@
 #include<stdio.h>
 
+// 简单线性回归算法
+// 利用最小二乘法思想实现，输入值分别为：整体数据样本，样本数
 float* regression(float **matrix,int num)
 {
+    // 默认最后得出公式为y=ax+b型
     float a,b;
+    // 分别计算x，y，x*y，x^2的和
     float x_sum=0,y_sum=0,xy_sum=0,xx_sum=0;
     float x_average=0,y_average=0;
 
@@ -16,10 +20,11 @@ float* regression(float **matrix,int num)
 
     x_average = x_sum/num;
     y_average = y_sum/num;
-
+    // 根据公式求出a，b的值
     b = (xy_sum - num*x_average*y_average)/(xx_sum-num*x_average*x_average);
     a = y_average - b * x_average;
-    
+
+    // 将a，b构成数组，作为参数返回
     float output[2]={a,b};
     float *po = output;
     return po;
@@ -48,6 +53,9 @@ int main()
     }
 
     float *result;
+
+    // 输入样本矩阵和样本数
     result = regression(p,30);
+    // 返回计算参数
     printf("SLR:在y=ax+b的情况下\na的值为%f\nb的值为%f",result[0],result[1]);
 }
